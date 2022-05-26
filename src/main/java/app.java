63801746382350c1809accf47eaedbf84665068a
Aditya Pathak <adityapathak@192.168.1.4>
2022-05-26 11:01:49 +0530

    initial commit

---
+++ src/main/java/app.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import java.util.Map;
+
+import main.resource.model.user;
+
+class app {
+    public static void main(String[] args) {
+        user[] users;
+        Map<Integer, Boolean> rollnums;
+        // load the users details from data to listOfUsers
+        // fill all the respected roll num using hashmap by iterating over the users
+        // give user options
+    }
+    
+}
